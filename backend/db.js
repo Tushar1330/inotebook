13bf69c47846c243c1646135a52bdb7b6076a0de
Tushar1330 <80581380+Tushar1330@users.noreply.github.com>
2023-10-11 16:13:15 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose  = require('mongoose');
-const mongoURI = "mongodb+srv://tushar:tushar12345@university.lsle9c0.mongodb.net/test?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv:/username:password@clustername.lsle9c0.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.set('strictQuery', false);
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
